1008baa757708503bca07f76cadd5a36d82a870f
Duncan Obare <95039318+Duncanobare@users.noreply.github.com>
2024-07-25 07:40:48 -0300

    Update donate.sol
    update

--- donate.sol
+++ donate.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.13;
 contract Donation {
         address payable owner;
-        uint256 balance;
+        uint256 totalDonations;
     struct Donor {
         address donor;
-        string name;
         uint256 amount;
     }
+    Donor dnr;
     Donor[] public donors;
     mapping(address => Donor) public getDonor;
-    event donated(address donor, string name, uint256 amount);
-    event received(address donor, string name, uint256 amount);
+    event donated(address donor, uint256 amount);
+    event received(address donor, uint256 amount);
     modifier onlyDonor() {
         require(owner != msg.sender, "only donor can donate");
         _;
@@ -25,7 +25,6 @@ contract Donation {
     }
     function donate(
         address _donor,
-        string memory _name,
         uint256 _amount
     ) public payable onlyDonor {
         require(msg.value % 1 ether == 0, "ETH tokens only!");
@@ -33,19 +32,21 @@ contract Donation {
         _amount = msg.value;
         Donor storage dn = getDonor[_donor];
         dn.donor = _donor;
-        dn.name = _name;
         dn.amount = _amount;
         owner.transfer(_amount);
-        emit donated(_donor, _name, _amount);
+        emit donated(_donor, _amount);
     }
-    function receiveDonations(address _donor) public payable onlyOwner returns (uint256) {
-        Donor storage dn = getDonor[_donor];
-        balance += dn.amount;
-        emit received(dn.donor, dn.name, dn.amount);
-        return balance;
+    function receiveDonations() public payable onlyOwner {
+        dnr = Donor(
+         msg.sender,msg.value
+        );
+        donors.push(dnr);
+        totalDonations += msg.value;
     }
-    function viewDonations(address _donor) public view returns (address, uint256, uint256) {
-        Donor storage dn = getDonor[_donor];
-        return (dn.donor, dn.amount, balance);
+    function getDonations() public view returns (Donor[] memory) {
+       return donors;
+    }
+    function gettotalDonations() public view returns (uint256){
+        return totalDonations;
     }
 }
