99be4ec0ee3923e5b7ddaeca223bfe16369e3230
Duncan Obare <95039318+Duncanobare@users.noreply.github.com>
2024-07-25 07:53:40 -0300

    fully functional

--- donate.sol
+++ donate.sol
@@ -10,33 +10,11 @@ contract Donation {
     Donor dnr;
     Donor[] public donors;
     mapping(address => Donor) public getDonor;
-    event donated(address donor, uint256 amount);
     event received(address donor, uint256 amount);
-    modifier onlyDonor() {
-        require(owner != msg.sender, "only donor can donate");
-        _;
-    }
-    modifier onlyOwner() {
-        require(owner == msg.sender, "only owner required");
-        _;
-    }
     constructor() {
         owner = payable(msg.sender);
     }
-    function donate(
-        address _donor,
-        uint256 _amount
-    ) public payable onlyDonor {
-        require(msg.value % 1 ether == 0, "ETH tokens only!");
-        require(msg.value > 0 ether, "amount must be greater than 0!");
-        _amount = msg.value;
-        Donor storage dn = getDonor[_donor];
-        dn.donor = _donor;
-        dn.amount = _amount;
-        owner.transfer(_amount);
-        emit donated(_donor, _amount);
-    }
-    function receiveDonations() public payable onlyOwner {
+    function receiveDonations() public payable {
         dnr = Donor(
          msg.sender,msg.value
         );
